ac36439fa1235180222953ff422e844185d5c75c
anonymous0010 <ctoxtli@gmail.com>
2017-09-23 02:17:33 -0500

    New changes

---
+++ app.py
@@ -0,0 +1,13 @@
+import tweepy, config
+
+class MyStreamListener(tweepy.StreamListener):
+	def on_status(self, status):
+		print(status)
+
+words = ["#happy"]
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+api = tweepy.API(auth)
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+myStream.filter(track=words, async=True)
